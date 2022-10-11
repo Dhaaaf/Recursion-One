fe723cabb1bc9adb21b6c0f12627ceedda3489e8
Nick <bassnlasky@gmail.com>
2022-10-11 11:16:37 -0700

    prob04

--- problems/04-range.js
+++ problems/04-range.js
@@ -11,11 +11,17 @@ range(7, 6); // []
 ***********************************************************************/
 
 
-function range(start, end) {
-  // Your code here
+function range(start, end, arr = []) {
+
+  if(end <= start){
+    return arr
+  }
+
+  arr.push(start)
+  return range (start + 1 , end, arr)
 }
 
-
+console.log(range(1, 5)); // [1, 2, 3, 4]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = range;
